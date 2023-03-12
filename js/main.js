7f1c41af01a1bf86f9e8ed09d72f0b502e66d035
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2023-03-13 02:34:35 +0530

    chat-bot-added

--- js/main.js
+++ js/main.js
@@ -36,3 +36,14 @@ document.addEventListener("DOMContentLoaded", function(){
   window.addEventListener("scroll", reveal);
 
 
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63fb754c31ebfa0fe7ef6a8b/1gq744dc9';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
